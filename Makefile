fa9126cb6ebc77ec03b1a1d83639476a52a5945a
Andrey S <mailhouse@list.ru>
2024-02-28 20:57:07 +0500

    feat: prepare files

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 install:
 	npm ci
 
+develop:
+	npx webpack serve --mode development --hot --open
+
+build:
+	NODE_ENV=production npx webpack
+
 publish:
 	npm publish --dry-run
 
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+  output: {
+    clean: true,
+  },
+};
